79e519e1d25a1e40f724368adb0608aeb079d7dc
brand-identity-brand <brand.identity.brand@gmail.com>
2024-06-22 22:57:22 -0400

    clear

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
-import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    dts({ include: ['lib'] })
-  ],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es']
-    },
-    copyPublicDir: false
-  }
+  plugins: [react()],
 })
