b797fa52f86f66db984d50f8e4d5bf41a873860d
brand-identity-brand <brand.identity.brand@gmail.com>
2024-06-22 23:41:12 -0400

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,11 +2,13 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
+    "eslint-config-prettier",
+    "plugin:storybook/recommended"
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'postcss.config.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+// name import
+export { Button } from './components/Button/'
+//default import
+// export { default as Button } from './components/Button/'
